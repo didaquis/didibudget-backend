ce75d03f0b4f5978520c141b50eeb9acbb736121
Dídac García <didaquis@gmail.com>
2020-03-07 21:26:42 +0100

    Updated dependencies

--- src/data/models/schemas/MonthlyBalanceSchema.js
+++ src/data/models/schemas/MonthlyBalanceSchema.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const uuidv4 = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
 
 /**
  * Monthly Balance schema
