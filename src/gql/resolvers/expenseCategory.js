bb1601179a400f67822caa52d662ab8ad0f98be8
Dídac García <didaquis@gmail.com>
2020-03-08 13:59:51 +0100

    Refactored resolver

--- src/gql/resolvers/expenseCategory.js
+++ src/gql/resolvers/expenseCategory.js
@@ -4,7 +4,7 @@ const { AuthenticationError } = require('apollo-server-express');
 
 const { logger } = require('../../utils/logger');
 
-const { Users, ExpenseCategory, ExpenseSubcategory } = require('../../data/models/index');
+const { ExpenseCategory } = require('../../data/models/index');
 const { authValidations } = require('../auth/validations');
 
 /**
@@ -14,18 +14,17 @@ const { authValidations } = require('../auth/validations');
 module.exports = {
 	Query: {
 		/**
-		 * Get all expense categories
+		 * Get all expense categories and subcategories
 		 */
 		getExpenseCategory: async (root, args, context) => {
-			// if (!authValidations.isLogged(context)) {
-			// 	throw new AuthenticationError('You must be logged in to perform this action');
-			// }
+			if (!authValidations.isLogged(context)) {
+				throw new AuthenticationError('You must be logged in to perform this action');
+			}
 
 			try {
 				const allExpenseCategories = await ExpenseCategory.find({ }, null, { sort: { name: 1 } }).populate('subcategories');
 
 				return allExpenseCategories || [];
-
 			} catch (error) {
 				logger.error(error);
 				return null;
