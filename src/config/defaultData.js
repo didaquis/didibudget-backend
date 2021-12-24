08e36a32ef0dbd4a878e76e6e47d06a487848748
Dídac García <didaquis@gmail.com>
2021-12-24 21:20:28 +0100

    Added the inmutableKey property to subcategories

--- src/config/defaultData.js
+++ src/config/defaultData.js
@@ -51,24 +51,31 @@ const expenseCategories = [
 		subcategories: [
 			{
 				name: 'Vehicle taxes, technical inspection',
+				inmutableKey: '2c0f7ea86b62582c2308'
 			},
 			{
 				name: 'Vehicle insurance',
+				inmutableKey: '55ba8bddc746c078ebc6'
 			},
 			{
 				name: 'Spare parts and maintenance',
+				inmutableKey: '0176bf56861cfc5ef72d'
 			},
 			{
 				name: 'Garage',
+				inmutableKey: '2bcd348acfe1bf3e7aec'
 			},
 			{
 				name: 'Vehicle cost (renting, leasing, vehicle itself)',
+				inmutableKey: '3fd1f7e8e2f828cbe212'
 			},
 			{
 				name: 'Fuel',
+				inmutableKey: '6f4ab3b2b1b3268d9138'
 			},
 			{
 				name: 'Tolls, parkings and tunnels',
+				inmutableKey: 'a54b87aab3bf45fa393b'
 			}
 		],
 	},
@@ -81,39 +88,51 @@ const expenseCategories = [
 		subcategories: [
 			{
 				name: 'Groceries, cleaning products, personal care products, objects, tools and cookware',
+				inmutableKey: '3e8e136c59a6d961c0cd'
 			},
 			{
 				name: 'Appliances and furniture',
+				inmutableKey: '67020bb5be8f6f358be9'
 			},
 			{
 				name: 'Electricity bill',
+				inmutableKey: '2795466d19a02edf91a2'
 			},
 			{
 				name: 'Water bill',
+				inmutableKey: '367152c689521a741912'
 			},
 			{
 				name: 'Gas bill',
+				inmutableKey: '09a4d2883e9605de3af5'
 			},
 			{
 				name: 'ISP provider bill',
+				inmutableKey: '649f5bae610a88cfafba'
 			},
 			{
 				name: 'Home taxes',
+				inmutableKey: 'd074eabc7b8e111bc6a5'
 			},
 			{
 				name: 'Home insurance',
+				inmutableKey: '79b8bc873e8aaf93e0d7'
 			},
 			{
 				name: 'Taxes of the community of neighbors',
+				inmutableKey: 'c9382b5849da8fb87c4b'
 			},
 			{
 				name: 'Repairs and home works',
+				inmutableKey: '6596560424b5a62054bb'
 			},
 			{
 				name: 'Mortgage',
+				inmutableKey: 'f6e025e8988d541e6e64'
 			},
 			{
 				name: 'Rent',
+				inmutableKey: '212d8beddfc1981bf3d2'
 			}
 		],
 	},
@@ -122,9 +141,11 @@ const expenseCategories = [
 		subcategories: [
 			{
 				name: 'English classes',
+				inmutableKey: '0cdbdd7f2050a17778c9'
 			},
 			{
 				name: 'Books, events, conferences, formative courses, certifications',
+				inmutableKey: 'db5fa3a569a72dca9079'
 			}
 		],
 	},
@@ -133,18 +154,23 @@ const expenseCategories = [
 		subcategories: [
 			{
 				name: 'Bar, restaurants and pubs',
+				inmutableKey: 'a90ae2812b970bd8d6f5'
 			},
 			{
 				name: 'Gym and climbing wall',
+				inmutableKey: '1f2db29fbcc94835f5ec'
 			},
 			{
 				name: 'Books, events, cinema, music, concerts, museums, and other cultural activities',
+				inmutableKey: '8764609f8c1456aa10ff'
 			},
 			{
 				name: 'Camping, shelter',
+				inmutableKey: 'd40c102031f4e79e23bc'
 			},
 			{
 				name: 'Recreational courses and formative sessions (cooking, alpinism...)',
+				inmutableKey: 'b2cbe8b45593c0b39b5e'
 			}
 		],
 	},
--- src/helpers/upsertDatabase.js
+++ src/helpers/upsertDatabase.js
@@ -19,12 +19,12 @@ const { ExpenseCategory, ExpenseSubcategory } = require('../data/models/index');
  * Save on the database the default data related to expense categories and subcategories. This function never delete documents on the dabatase, but should generate a new document if content of default data change
  * @param {Array.<Object>} expenseCategories - A list of literal objects
  * @param {string} expenseCategories.name - Name of expense category
- * @param {Array.<{name: string}>} expenseCategories.subcategories - A list of literal objects. Every object should contain the property name. Can be an empty array
+ * @param {Array.<{name: string, inmutableKey: string}>} expenseCategories.subcategories - A list of literal objects. Every object should contain the properties name and inmutableKey. May be an empty array
  */
 const upsertDBWithExpenseCategories = ({ expenseCategories } = []) => {
 	expenseCategories.forEach(async (category) => {
 		const upsertSubcategories = category.subcategories.map((subcategory) => {
-			return ExpenseSubcategory.findOneAndUpdate({ name: subcategory.name }, { name: subcategory.name }, { upsert: true, new: true, setDefaultsOnInsert: true });
+			return ExpenseSubcategory.findOneAndUpdate({ name: subcategory.name }, { name: subcategory.name, inmutableKey: subcategory.inmutableKey }, { upsert: true, new: true, setDefaultsOnInsert: true });
 		});
 
 		const listOfSubcategories = await Promise.all(upsertSubcategories);
