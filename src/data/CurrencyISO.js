0a653fefa77068fddbda1ac75a558b87a38116e6
Dídac García <didaquis@gmail.com>
2025-10-08 20:46:55 +0200

    Add a new file

---
+++ src/data/CurrencyISO.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Available currencies
+ * @readonly
+ * @enum {string}
+ * @property {string} EUR    	- EUR
+ */
+const CurrencyISO = Object.freeze({
+	EUR: 'EUR',
+});
+
+module.exports = { CurrencyISO };
