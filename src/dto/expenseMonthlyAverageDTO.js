96aac5cbbe6d200163d6c568330516fd573dc5be
Dídac García <didaquis@gmail.com>
2025-11-28 02:06:35 +0100

    Improve docs

--- src/dto/expenseMonthlyAverageDTO.js
+++ src/dto/expenseMonthlyAverageDTO.js
@@ -3,7 +3,7 @@
 const { roundToTwoDecimals } = require('../helpers/roundToTwoDecimals');
 
 /**
- * Builds a DTO of expense
+ * Builds a DTO of ExpensesMonthlyAverage
  * 	@param {number} average
  *  @param {string} currencyISO
  * @returns {Object}
